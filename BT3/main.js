dee97a932b7fdae33d846dfcd72c8c5139af680f
flyingdoggoo <nguyenthanhhieu17022005@gmail.com>
2025-01-10 01:47:20 +0700

    cm

--- BT3/main.js
+++ BT3/main.js
@@ -1,12 +1,12 @@
 const cards = document.querySelectorAll('.card');
+const paragraph = ["Bulbasaur là Pokémon hệ Grass và Poison với hạt giống trên lưng. Nó hấp thụ ánh sáng mặt trời để phát triển và là một trong những Pokémon khởi đầu ở vùng Kanto.",
+    "Ivysaur là Pokémon hệ Grass và Poison, tiến hóa từ Bulbasaur. Bông hoa trên lưng của nó bắt đầu nở, báo hiệu sự trưởng thành và sắp sửa tiến hóa tiếp.",
+    "Venusaur là Pokémon hệ Grass và Poison, tiến hóa từ Ivysaur. Bông hoa trên lưng của nó phát triển lớn hơn và phát ra mùi hương đặc biệt.",
+    "Charmander là Pokémon hệ Fire, nổi bật với ngọn lửa trên đuôi. Nó mạnh mẽ và kiên cường, phát triển thành Charmeleon và sau đó là Charizard khi đạt đủ cấp độ.",
+    "Charmeleon là Pokémon hệ Fire, tiến hóa từ Charmander. Nó trở nên mạnh mẽ hơn với sức nóng dữ dội và khả năng kiểm soát lửa tốt hơn khi chiến đấu.",
+    "Charizard là Pokémon hệ Fire và Flying, tiến hóa từ Charmeleon. Nó có khả năng bay và phun lửa từ miệng, là một trong những Pokémon mạnh nhất ở vùng Kanto.",
+]
 function pokeDes(index){
-    const paragraph = ["Bulbasaur là Pokémon hệ Grass và Poison với hạt giống trên lưng. Nó hấp thụ ánh sáng mặt trời để phát triển và là một trong những Pokémon khởi đầu ở vùng Kanto.",
-        "Ivysaur là Pokémon hệ Grass và Poison, tiến hóa từ Bulbasaur. Bông hoa trên lưng của nó bắt đầu nở, báo hiệu sự trưởng thành và sắp sửa tiến hóa tiếp.",
-        "Venusaur là Pokémon hệ Grass và Poison, tiến hóa từ Ivysaur. Bông hoa trên lưng của nó phát triển lớn hơn và phát ra mùi hương đặc biệt.",
-        "Charmander là Pokémon hệ Fire, nổi bật với ngọn lửa trên đuôi. Nó mạnh mẽ và kiên cường, phát triển thành Charmeleon và sau đó là Charizard khi đạt đủ cấp độ.",
-        "Charmeleon là Pokémon hệ Fire, tiến hóa từ Charmander. Nó trở nên mạnh mẽ hơn với sức nóng dữ dội và khả năng kiểm soát lửa tốt hơn khi chiến đấu.",
-        "Charizard là Pokémon hệ Fire và Flying, tiến hóa từ Charmeleon. Nó có khả năng bay và phun lửa từ miệng, là một trong những Pokémon mạnh nhất ở vùng Kanto.",
-    ]
     const card = cards[index]
     const img = card.querySelector('img')
     const description = card.querySelector('.description')
@@ -35,5 +35,5 @@ function pokeDes(index){
 }
 
 cards.forEach((card, index) => {
-    card.addEventListener('mousedown', () => pokeDes(index));
+    card.addEventListener('click', () => pokeDes(index));
 });
